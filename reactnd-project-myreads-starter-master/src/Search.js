aaf0f8d5ef869afeb518598c9bc0f3c225db9b5e
samuel monai <samuelmonai@hotmail.com>
2018-03-21 20:37:50 -0300

    Estante, pagina pesquisa

---
+++ reactnd-project-myreads-starter-master/src/Search.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import {Link} from 'react-router-dom'
+import Shelf from './Shelf'
+
+
+class Search extends React.Component {
+  constructor(){
+    super();
+      this.state={query:'',queryResults:[],myBooks:[],myBooksOnDisplay:[]
+    };
+    this.onChange = (ev)=>{
+      const state = Object.assign({},this.state);
+      const campo = ev.target.name;
+      state[campo]= ev.target.value;
+      this.setState(state);
+    };
+  }
+
+  moveBook = (book,shelf) => {
+    this.props.onMoveBook(book,shelf)
+    shelf!=='none' ?
+      (this.setState((state)=>({
+        myBooksOnDisplay: state.myBooksOnDisplay.concat(book),
+        queryResults:state.queryResults.filter((b) => b.id !== book.id),
+        myBooks: state.myBooks.concat(book)
+      })))
+    :
+      (this.setState((state)=>({
+        myBooksOnDisplay: state.myBooksOnDisplay.filter((b) => b.id !== book.id),
+        queryResults: state.queryResults.concat(book),
+        myBooks: state.myBooks.filter((b) => b.id !== book.id)
+      })))
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((myBooks)=>{
+      this.setState({myBooks:myBooks})
+    })
+  }
+
+  updateQuery = (query) =>{
+    this.setState({query:query})
+      BooksAPI.search(this.state.query).then((books)=>{
+      if (books) {
+        let interse =[]
+        for (const book of books) {
+          for ( const b of this.state.myBooks) {
+            if (b.id === book.id){
+              book.shelf=b.shelf;
+              interse.push(book);
+              books = books.filter((b) => (b.id!==book.id))
+            }
+          }
+        }
+        this.setState({myBooksOnDisplay:interse})
+        this.setState({queryResults:books})
+      }
+    })
+  }
+
+  render(){
+    let myBooksOnDisplay =[]
+    let queryResults = []
+    if (this.state.query) {
+      myBooksOnDisplay = this.state.myBooksOnDisplay
+      queryResults = this.state.queryResults
+    }
+    return(
+      <div className="search-books">
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">Close</Link>
+        <div className="search-books-input-wrapper">
+          {           
+            /*
+            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+            You can find these search terms here:
+            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+            you don't find a specific author or title. Every search is limited by search terms.
+          */}
+          <input type="text" placeholder="Search by title or author" value={this.state.query} 
+          onChange={(event)=>this.updateQuery(event.target.value)}/>
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+        <div className="list-books-content">
+          <Shelf onMoveBook={this.moveBook} myBooks={myBooksOnDisplay} shelf={'On My Colection'}/>
+          <Shelf onMoveBook={this.moveBook} myBooks={queryResults} shelf={'Looking'}/>
+      </div >
+      </div>
+    </div>
+    )
+  }
+}
+export default Search
---
+++ reactnd-project-myreads-starter-master/src/Shelf.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Shelf (props) {
+    return(
+      <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.shelf}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.myBooks.map((book)=>(
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{
+                    width: 128, height: 193,
+                    backgroundImage:book.imageLinks ? `url(${book.imageLinks.smallThumbnail})` :null
+                    }}>
+                  </div>
+                  <div className="book-shelf-changer">
+                    <select value={book.shelf} onChange={(event)=>props.onMoveBook(book,event.target.value)}>
+                    <option value="none">None</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    </select>
+                  </div>
+                </div>
+               <div className="book-title">{book.title}</div>
+               <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  )
+}
+export default Shelf
